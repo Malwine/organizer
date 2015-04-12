8a99bfb90855b6ab7c536ef1ced9bdef6afad697
Malwine <malwine@mailbox.org>
2015-04-12 14:12:10 +0200

    Add second spec and task model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Project do
+describe Project do
   it "considers a project with no tasks to be done" do
     project = Project.new
     expect(project.done?).to be_truthy
+  end
+
+  it "is not done with an incomplete task" do
+    project = Project.new
+    task = Task.new
+    project.tasks << task
+    expect(project.done?).to be_falsy
   end
 end
